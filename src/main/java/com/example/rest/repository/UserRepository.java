d5d0eb3f7c7ca466dabb906c955fb7e81c1f2065
lamasurfer <7376771@gmail.com>
2021-02-26 12:31:51 +0300

    initial commit

---
+++ src/main/java/com/example/rest/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.rest.repository;
+
+import com.example.rest.model.Authorities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+
+    public List<Authorities> getUserAuthorities(String user, String password) {
+        return new ArrayList<>();
+    }
+
+}
